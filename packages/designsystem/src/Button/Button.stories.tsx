f5ff7a50b9aa9b38b56d78282d06447a6e7214c1
Marcony Neves <marconygabriel.pinto@adidas.com>
2023-04-11 21:03:32 +0200

    chore: adjust storybook integration

--- packages/designsystem/src/Button/Button.stories.tsx
+++ packages/designsystem/src/Button/Button.stories.tsx
@@ -4,7 +4,7 @@ import { Button, Sizes } from "./Button";
 
 // More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
 const meta: Meta<typeof Button> = {
-  title: "Button",
+  title: "Example/Button",
   component: Button,
   tags: ["autodocs"],
   argTypes: {

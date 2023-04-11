a9443dc044fab24b522aa41658933b5d3a515305
Marcony Neves <marconygabriel.pinto@adidas.com>
2023-04-11 22:34:32 +0200

    chore: add dummy component tests

---
+++ packages/designsystem/src/Button/__tests__/Button.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+
+import { Button } from "../Button";
+
+describe("<Button />", () => {
+  const defaultProps = { label: "Button" };
+  test("render", () => {
+    const { asFragment } = render(<Button {...defaultProps} />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+  it("should render as primary state", () => {
+    render(<Button {...defaultProps} primary />);
+
+    const btn = screen.getByRole("button");
+
+    expect(btn).toHaveClass("storybook-button--primary");
+  });
+  it("should change backgroundColor", () => {
+    render(<Button {...defaultProps} backgroundColor="red" />);
+
+    const btn = screen.getByRole("button");
+
+    expect(btn).toHaveStyle({ backgroundColor: "red" });
+  });
+});

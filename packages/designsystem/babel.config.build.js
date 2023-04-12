d62d7f9353d803477ba0f37e3ef24a09b11d8d4d
Marcony Neves <marconygabriel.pinto@adidas.com>
2023-04-12 20:24:54 +0200

    chore: Update build mechanics

---
+++ packages/designsystem/babel.config.build.js
@@ -0,0 +1,3 @@
+module.exports = {
+  extends: "@monorepo/internal/babel/build.js",
+};
--- packages/designsystem/src/Button/__tests__/Button.test.tsx
+++ packages/designsystem/src/Button/__tests__/Button.test.tsx
@@ -1,10 +1,11 @@
+import React from "react";
 import { render, screen } from "@testing-library/react";
 
 import { Button } from "../Button";
 
 describe("<Button />", () => {
   const defaultProps = { label: "Button" };
-  test("render", () => {
+  it("render", () => {
     const { asFragment } = render(<Button {...defaultProps} />);
 
     expect(asFragment()).toMatchSnapshot();

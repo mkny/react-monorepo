b9797b010b74abdc443ec5f984adeba14980d2ee
Marcony Neves <marconygabriel.pinto@adidas.com>
2023-04-13 12:00:31 +0200

    chore: update tests and typing

--- apps/react-app/src/App.test.tsx
+++ apps/react-app/src/App.test.tsx
@@ -1,9 +1,22 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { logger } from "@monorepo/corelib";
 
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock("@monorepo/corelib");
+
+describe("<App /> tests", () => {
+  it("render", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+  it("should be able to click and get a log", () => {
+    render(<App />);
+    const btn = screen.getByText(/my world/);
+
+    fireEvent.click(btn);
+
+    expect(logger).toHaveBeenCalledTimes(1);
+  });
 });
--- packages/corelib/src/index.ts
+++ packages/corelib/src/index.ts
@@ -1,2 +1,2 @@
 // eslint-disable-next-line no-console
-export const logger = (...args: any[]) => console.log.apply(this, args);
+export const logger = (...args: unknown[]) => console.log.apply(this, args);

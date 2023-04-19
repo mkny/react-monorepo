4b79fd9199d7add7a20f130b77fd4a2158e2037a
Marcony Neves <marconyneves@gmail.com>
2023-04-19 11:54:50 -0300

    fix(monorepo): update lint-staged script

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,6 +2,6 @@ module.exports = {
   "*.{js,jsx,ts,tsx}": [
     "pretty-quick --staged",
     () => "npm run lint:fix",
-    "npm run test:ci",
+    () => "npm run test:ci -- --onlyChanged",
   ],
 };
